20e0b7e5723c055706e3dda017ba9b5efc71cf0f
Levhita <142937923+levhitaIncode@users.noreply.github.com>
2024-09-17 19:44:30 -0600

    Add AES flow to sample (#3)
    * * added eslint
    * added ability to save identityId
    * updated apiurl to /0
    
    * added upload document capabiity and get download link at the end
    
    * moved frontend to it's own folder
    
    * added integrated backend into the project
    
    * general improved ui and messaging

---
+++ backend/index.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const fs = require('node:fs');
+
+const app = express();
+dotenv.config();
+
+app.use(cors())
+// Middleware to handle raw body data
+app.use(express.raw({ type: '*/*' }));
+
+const defaultHeader = {
+  'Content-Type': "application/json",
+  'x-api-key': process.env.API_KEY,
+  'api-version': '1.0'
+};
+
+// Admin Token + ApiKey are needed for approving
+const adminHeader = {
+  'Content-Type': "application/json",
+  'x-api-key': process.env.API_KEY,
+  'X-Incode-Hardware-Id': process.env.ADMIN_TOKEN,
+  'api-version': '1.0'
+};
+
+// Receives the information about a faceMatch attempt and verifies
+// if it was correct and has not been tampered.
+app.post('/verify', async (req, res) => {
+  /** Get parameters from body */
+  const faceMatchData = JSON.parse(req.body.toString());
+  const {transactionId, token, interviewToken} = faceMatchData;
+  const verificationParams = { transactionId, token, interviewToken };
+  
+  let response={};
+  try{
+    /** Run Call against incode API **/
+    const verifyAttemptUrl = `${process.env.API_URL}/omni/authentication/verify`;
+    response = await doPost(verifyAttemptUrl, verificationParams, adminHeader);
+  } catch(e) {
+    console.log(e.message);
+    res.status(500).send({success:false, error: e.message});
+    return;
+  }
+  log = {
+    timestamp: new Date().toISOString().slice(0, 19).replace('T', ' '),
+    data: {verificationParams, response}
+  }
+  res.status(200).send(response);
+  
+  // Write to a log so you can debug it.
+  console.log(log);
+});
+
+// if it was correct and has not been tampered.
+app.post('/sign', async (req, res) => {
+  /** Receive contract and token as parameters */
+  const signParamsData = JSON.parse(req.body.toString());
+  const { interviewToken, base64Contract } = signParamsData;
+  
+  /** Prepare the authorization header that will be used in calls to incode */
+  sessionHeader = {...defaultHeader};
+  sessionHeader['X-Incode-Hardware-Id'] = interviewToken;
+  
+  let response = {};
+  try{
+    /** Get URL where to upload the contract */
+    const generateDocumentUploadUrl = `${process.env.API_URL}/omni/es/generateDocumentUploadUrl`;
+    const documentURLData = await doPost(generateDocumentUploadUrl, { token:interviewToken }, sessionHeader);
+    const {referenceId, preSignedUrl} = documentURLData
+    
+    /** Upload contract to AWS presigned url */
+    const binary = Buffer.from(base64Contract, 'base64');
+    const uploadResponse =  await fetch(preSignedUrl, {
+      method: "PUT",
+      headers: {"Content-Type": "application/pdf"},
+      body: binary,
+    })
+    if (!uploadResponse.ok) {
+      throw new Error('Uploading contract failed with code ' + uploadResponse.status)
+    }
+    
+    /** Sign the document */
+    const signURL = `${process.env.API_URL}/omni/es/process/sign`;
+    const signData = await doPost(signURL,
+      {
+        "documentRef": referenceId,
+        "userConsented": true
+      },
+      sessionHeader
+    );
+    const {success} = signData
+    if (!success) { throw new Error('Sign failed');}
+    
+    /** Fetch all signed document references */
+    const documentsSignedURL = `${process.env.API_URL}/omni/es/documents/signed`;
+    const documentsSignedData = await doGet(documentsSignedURL, {}, sessionHeader);
+    const {documents} = documentsSignedData
+    
+    /** This endpoint returns all documents, find the one just signed  matching by referenceId*/
+    const justSigned = documents.find(document => document.documentRef=== referenceId)
+    
+    /** Return referenceId and documentUrl */
+    const {documentRef, documentUrl} = justSigned
+    response = {referenceId, documentUrl}
+    
+  } catch(e) {
+    console.log(e.message);
+    res.status(500).send({success:false, error: e.message});
+    return;
+  }
+  log = {
+    timestamp: new Date().toISOString().slice(0, 19).replace('T', ' '),
+    data: {signParamsData, response}
+  }
+  res.status(200).send(response);
+  // Write to a log so you can debug it.
+  console.log(log);
+});
+
+
+app.get('*', function(req, res){
+  res.status(404).json({error: `Cannot GET ${req.url}`});
+});
+
+app.post('*', function(req, res){
+  res.status(404).json({error: `Cannot POST ${req.url}`});
+});
+
+// Utility functions
+const doPost = async (url, bodyparams, headers) => {
+  try {
+    const response = await fetch(url, { method: 'POST', body: JSON.stringify(bodyparams), headers});
+    if (!response.ok) {
+      //console.log(response.json());
+      throw new Error('Request failed with code ' + response.status)
+    }
+    return response.json();
+  } catch(e) {
+    console.log({url, bodyparams, headers})
+    throw new Error('HTTP Post Error: ' + e.message)
+  }
+}
+
+const doGet = async (url, params, headers) => {
+  try {
+    const response = await fetch(`${url}?` + new URLSearchParams(params), {method: 'GET', headers});
+    if (!response.ok) {
+      //console.log(await response.json());
+      throw new Error('Request failed with code ' + response.status)
+    }
+    return response.json();
+  } catch(e) {
+    console.log({url, params, headers})
+    throw new Error('HTTP Get Error: ' + e.message)
+  }
+}
+
+
+// Listen for HTTP
+const httpPort = 3000;
+
+app.listen(httpPort, () => {
+  console.log(`HTTP listening on: http://localhost:${httpPort}/`);
+});
+
+module.exports = app;
---
+++ frontend/main.js
@@ -0,0 +1,126 @@
+const ServerUrl = import.meta.env.VITE_SERVER_URL
+/** Imports incode instance added with script tag **/
+let onBoarding
+
+const mainContainer = document.getElementById('app')
+const loginContainer = document.getElementById('login')
+const loginButton = document.getElementById('login-button')
+
+function showError (e = null) {
+  mainContainer.innerHTML = 'Something Went Wrong, see console for details...'
+  console.log(e)
+}
+
+function identifyUser (identityId) {
+  onBoarding.renderLogin(mainContainer, {
+    onSuccess: async (response) => {
+      const { token, transactionId, interviewToken, faceMatch, customerId, email } = response
+      if (faceMatch) {
+        /**  User has an Incode Identity.
+         * Verify using your backend that the faceMatch was actually valid and
+         * not man in the middle attack */
+        const response = await fetch(`${ServerUrl}/verify`,
+          {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({ token, transactionId, interviewToken })
+          }
+        )
+        const verification = await response.json()
+        if (verification.verified === true) {
+          finish(customerId, email, interviewToken)
+        } else {
+          showError(new Error('FaceMatch is invalid.'))
+        }
+      } else {
+        showError(new Error('Face did not match any user.'))
+      }
+    },
+    onError: error => {
+      showError(error)
+    // User not found. Add rejection your logic here
+    },
+    isOneToOne: true,
+    oneToOneProps: {
+      identityId
+    }
+  })
+}
+
+function finish (customerId, email, interviewToken) {
+  mainContainer.innerHTML = `Sucessfull Login:<br/>\n<div>CustomerId: ${customerId}</div>\n<div>Email: ${email}</div><br/><input type="file" name="upload" id="file-input" accept="application/pdf" /><button id="sign-button">Sign</button>`
+  const fileInput = document.getElementById('file-input')
+  const signButton = document.getElementById('sign-button')
+
+  signButton.addEventListener('click', async () => {
+    const file = fileInput.files[0]
+    const base64Contract = await toBase64(file)
+    sign(interviewToken, base64Contract.replace('data:application/pdf;base64,', ''))
+  })
+}
+
+async function sign (interviewToken, base64Contract) {
+  const signButton = document.getElementById('sign-button')
+  signButton.disabled = true
+  signButton.innerHTML = 'Signing...'
+  const response = await fetch(`${ServerUrl}/sign`,
+    {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ interviewToken, base64Contract })
+    }
+  )
+
+  if (!response.ok) {
+    console.log(response)
+    showError("Couldn't Sign")
+  }
+
+  const signData = await response.json()
+  const { referenceId, documentUrl } = signData
+  mainContainer.innerHTML = `Sucessfull Signature:<br/>\n<div>refenceId: ${referenceId}</div>\n<div>DocumentUrl: <a href="${documentUrl}">Download</a></div>`
+}
+
+function toBase64 (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = reject
+  })
+}
+
+async function app () {
+  // Create the instance of incode linked to a client
+  const apiURL = import.meta.env.VITE_API_URL
+  // Enable for 1:N
+  // const clientId = import.meta.env.VITE_CLIENT_ID;
+  // const apiKey = import.meta.env.VITE_API_KEY;
+
+  const identityIdInput = document.getElementById('identity-id')
+
+  onBoarding = window.OnBoarding.create({
+    apiURL
+    // clientId, // Enable for 1:N
+    // apiKey // Enable for 1:N
+  })
+
+  // Get it from localstorage so dev doesn't have to type it everytime
+  const identityId = localStorage.getItem('identityId')
+  if (identityId) {
+    identityIdInput.value = identityId
+  }
+
+  // Empty the message and starting the flow
+  mainContainer.innerHTML = ''
+  loginContainer.style.display = 'flex'
+  loginButton.addEventListener('click', () => {
+    loginContainer.style.display = 'none'
+
+    // Save it to localstorage so dev doesn't have to type it everytime
+    localStorage.setItem('identityId', identityIdInput.value)
+
+    identifyUser(identityIdInput.value)
+  })
+}
+document.addEventListener('DOMContentLoaded', app)
